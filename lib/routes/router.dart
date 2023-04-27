65ff0c178579a3725927a72f770f0f3d9f7c1e5e
mubarismdeen <mubarisv@gmail.com>
2023-04-28 00:38:11 +0530

    changing router animation style

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -25,5 +25,19 @@ Route<dynamic> generateRoute(RouteSettings settings){
 }
 
 PageRoute _getPageRoute(Widget child){
-  return MaterialPageRoute(builder: (context) => child);
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
